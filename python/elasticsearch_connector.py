3cef347efe29f91c40803777c0c76163869526ea
Johannes Demel <demel@ant.uni-bremen.de>
2021-02-08 14:29:06 +0100

    elasticsearch: Switch to ElasticBatch
    We hope to reduce stress on the DB with bulk transfers. ElasticBuffer
    should take care of this.

--- python/elasticsearch_connector.py
+++ python/elasticsearch_connector.py
@@ -1,54 +1,85 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020 Johannes Demel.
+# Copyright 2020, 2021 Johannes Demel.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
 
 import numpy
-import time
-from gnuradio import gr
-import elasticsearch
-import pmt
 import datetime
+import elasticsearch
+from elasticbatch import ElasticBuffer
+from gnuradio import gr
+import pmt
+
 
 class elasticsearch_connector(gr.sync_block):
     """
-    docstring for block elasticsearch_connector
+    ElasticSearch Connector.
+
+    Send flowgraph state data to a database.
+    Here we need to
+    1. Connect to the correct database
+    2. Set some metadata correctly
+    3. Format data correctly to be sent to the database.
+    4. Send data. Take care of correct transmission properties.
     """
-    def __init__(self, hostname='localhost', port=9200):
+
+    def __init__(self, hostname='localhost', port=9200, device_id=4711,
+                 data_type='5gdata', index_prefix='measurements-',
+                 buffer_size=5000):
         gr.sync_block.__init__(self,
-            name="elasticsearch_connector",
-            in_sig=[],
-            out_sig=[])
+                               name="elasticsearch_connector",
+                               in_sig=[],
+                               out_sig=[])
         self.message_port_register_in(pmt.mp("in"))
         self.set_msg_handler(pmt.mp("in"), self.handle_msg)
-        dbname = f'{hostname}:{port}'
-        self._db = elasticsearch.Elasticsearch([dbname, ])
-        print(self._db.info())
 
-        self._db_device_id = f'{4711}'
-        self._db_data_type = f'5gdata'
+        # set up database connection
+        dbname = f'{hostname}:{port}'
+        client_kwargs = {'hosts': dbname}
+
+        # Configure common metadata
+        self._db_device_id = f'{device_id}'
+        self._db_data_type = f'{data_type}'
         self._common_body = {'type': self._db_data_type,
                              'deviceId': self._db_device_id}
-        self._db_index = f'measurements-next-{datetime.date.today()}'
+
+        # Configure database.
+        self._db_index = f'{index_prefix}{datetime.date.today()}'
         self._db_doc_type = 'doc'
 
-    def send_to_db(self, body):
-        body.update(self._common_body)
-        body['timestamp'] = datetime.datetime.now().timestamp()
-        self._db.index(self._db_index, doc_type=self._db_doc_type, body=body)
+        def index_func(doc):
+            return self._db_index
 
+        def data_type_func(doc):
+            return self._db_data_type
+
+        def device_id_func(doc):
+            return self._db_device_id
+
+        def timestamp_func(doc):
+            return int(1e3 * datetime.datetime.now().timestamp())
+
+        self._db_buffer = ElasticBuffer(client_kwargs=client_kwargs,
+                                        _index=index_func,
+                                        type=data_type_func,
+                                        deviceId=device_id_func,
+                                        timestamp=timestamp_func,
+                                        size=buffer_size)
+
+    def send_to_buffer(self, body):
+        self._db_buffer.add([body, ])
+        # self._db_buffer.flush()
 
     def handle_msg(self, msg):
-        idx = pmt.symbol_to_string(pmt.car(msg))
+        # meta = pmt.symbol_to_string(pmt.car(msg))
         values = pmt.cdr(msg)
 
         if not pmt.is_dict(values):
-            self.send_to_db({"foo": pmt.to_python(values)})
-            # self._db.index(self._db_index, doc_type=self._db_doc_type, body={"foo": str(pmt.to_python(values))})
+            self.send_to_buffer({"foo": pmt.to_python(values)})
             return
 
         body = {}
@@ -61,14 +92,9 @@ class elasticsearch_connector(gr.sync_block):
                     body[f'qos.{k}.imag'] = pmt.to_python(v).imag
                 else:
                     body[f'qos.{k}'] = pmt.to_python(v)
-
-        self.send_to_db(body)
-        # self._db.index(self._db_index, doc_type=self._db_doc_type, body=body)
-
+        self.send_to_buffer(body)
 
     def work(self, input_items, output_items):
-        in0 = input_items[0]
-        out = output_items[0]
-        # <+signal processing here+>
-        out[:] = in0
+        # No signal processing! This should never happen!
+        # This is a message only block!
         return len(output_items[0])
